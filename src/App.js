1c38030cd6e4b36ec63edc4038f53d8206972deb
ramirez <prlara2000@gmail.com>
2021-08-08 20:39:25 -0500

    add video

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import "./App.css";
 import SearchInput from "./components/SearchInput";
 import axios from "axios";
 import Header from "./components/Header";
-import Button from "./components/Button";
+import save from "./images/save.svg";
+import vote from "./images/vote.svg";
 
 class App extends Component {
   constructor(props) {
@@ -18,23 +19,28 @@ class App extends Component {
     this.handleSearch = this.handleSearch.bind(this);
   }
 
-  handleDescription(id) {
-    const options = {
-      method: "GET",
-      url: `https://data-imdb1.p.rapidapi.com/movie/id/${id}/`,
-      headers: {
-        "x-rapidapi-key": "c426a80468msh489383a1e8c815ap17e034jsn7fba12f2df64",
-        "x-rapidapi-host": "data-imdb1.p.rapidapi.com",
-      },
-    };
-    axios
-      .request(options)
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
+  handleDescription(titles) {
+    const arr = [];
+    titles.forEach((movie) => {
+      const options = {
+        method: "GET",
+        url: `https://data-imdb1.p.rapidapi.com/movie/id/${movie.imdb_id}/`,
+        headers: {
+          "x-rapidapi-key":
+            "c426a80468msh489383a1e8c815ap17e034jsn7fba12f2df64",
+          "x-rapidapi-host": "data-imdb1.p.rapidapi.com",
+        },
+      };
+      axios
+        .request(options)
+        .then((response) => {
+          arr.push(response.data[movie.title]);
+          this.setState({ description: arr });
+        })
+        .catch(function (error) {
+          console.error(error);
+        });
+    });
   }
 
   handleSearch(e) {
@@ -60,6 +66,7 @@ class App extends Component {
         .then((response) => {
           if (response.data.Result.length > 0) {
             this.setState({ titles: response.data.Result });
+            this.handleDescription(this.state.titles);
           } else {
             alert("Please check your spelling and try again!");
           }
@@ -83,21 +90,55 @@ class App extends Component {
         <Header />
         <hr className="hr" />
         <SearchInput search={this.handleSearch} />
-        <ol>
-          {this.state.titles.map((movie) => {
+        <div>
+          {this.state.description.map((movie) => {
             return (
-              <li key={movie.imdb_id} className="moviesList">
-                {movie.title}
-                <span className="spanButton">
-                  <Button
-                    description={this.handleDescription}
-                    movieId={movie.imdb_id}
+              <div key={movie.imdb_id} className="moviesList">
+                <hr className="hr" />
+                <div className="topDescription">
+                  <div>
+                    <h1 className="topTitle">{movie.title}</h1>
+                    <h4>
+                      {movie.year} &nbsp;
+                      {movie.gen[0].genre} &nbsp;
+                      {movie.content_rating}
+                    </h4>
+                    <h5>running time {movie.movie_length}</h5>
+                    <h5 className="topPlot">{movie.plot}</h5>
+                    <iframe
+                      className="trailer"
+                      title={movie.title}
+                      src={movie.trailer}
+                    ></iframe>
+                    <div className="saveAndVote">
+                      <div className="topSave">
+                        <img className="saveIcon" src={save} alt="save icon" />
+                        <h5>Save</h5>
+                      </div>
+                      <div className="topVote">
+                        <img className="saveIcon" src={vote} alt="save icon" />
+                        <h5>Vote</h5>
+                      </div>
+                    </div>
+                  </div>
+
+                  <img
+                    className="banner"
+                    src={movie.banner}
+                    alt="movie banner"
                   />
-                </span>
-              </li>
+                </div>
+
+                <p className="bottomDescription">{movie.description}</p>
+                <div className="bottomFacts">
+                  <h4>Popularity: {movie.popularity}</h4>
+                  <h4>Release date: {movie.release}</h4>
+                  <h4>Rating: {movie.rating}</h4>
+                </div>
+              </div>
             );
           })}
-        </ol>
+        </div>
       </div>
     );
   }
